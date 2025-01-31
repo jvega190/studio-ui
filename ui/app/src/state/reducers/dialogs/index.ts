414e0886ba0f277729b84fe1cdbc9aa019ae2396
jvega190 <josevega19@gmail.com>
2025-01-31 16:04:07 -0600

    [dialogs-system] Remove reducers

--- ui/app/src/state/reducers/dialogs/index.ts
+++ ui/app/src/state/reducers/dialogs/index.ts
@@ -15,70 +15,44 @@
  */
 
 import { combineReducers } from 'redux';
-import confirm from './confirm';
 import error from './error';
-import newContent from './newContent';
 import minimizedTabs from './minimizedTabs';
 import history from './history';
 import viewVersion from './viewVersion';
 import compareVersions from './compareVersions';
-import publish from './publish';
-import dependencies from './dependencies';
 import deleteDialog from './delete';
 import edit from './edit';
 import codeEditor from './codeEditor';
 import workflowCancellation from './workflowCancellation';
 import reject from './reject';
-import createFolder from './createFolder';
-import createFile from './createFile';
-import copy from './copy';
-import upload from './upload';
-import singleFileUpload from './singleFileUpload';
-import preview from './preview';
 import editSite from './editSite';
 import pathSelection from './pathSelection';
-import changeContentType from './changeContentType';
 import itemMenu from './itemMenu';
 import itemMegaMenu from './itemMegaMenu';
 import launcher from './launcher';
-import publishingStatus from './publishingStatus';
 import unlockPublisher from './unlockPublisher';
-import widget from './widget';
 import uiBlocker from './uiBlocker';
 import renameAsset from './renameAsset';
 import brokenReferences from './brokenReferences';
 
 export default combineReducers({
-  confirm,
   error,
-  newContent,
   minimizedTabs,
   history,
   viewVersion,
   compareVersions,
-  publish,
-  dependencies,
   delete: deleteDialog,
   edit,
   codeEditor,
   workflowCancellation,
   reject,
   editSite,
-  createFolder,
-  createFile,
   renameAsset,
-  copy,
-  upload,
-  singleFileUpload,
-  preview,
   pathSelection,
-  changeContentType,
   itemMenu,
   itemMegaMenu,
   launcher,
-  publishingStatus,
   unlockPublisher,
-  widget,
   uiBlocker,
   brokenReferences
 });
